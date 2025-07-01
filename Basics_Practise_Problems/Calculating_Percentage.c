a5a97ccf4d5d597a211079707dfd43831ae82472
rohit528590 <rohit528590@icloud.com>
2025-07-01 07:25:32 +0530

    Practice Problem Based On Basics of C

--- Basics_Practise_Problems/Calculating_Percentage.c
+++ Basics_Practise_Problems/Calculating_Percentage.c
@@ -1,1 +1,24 @@
-// Calculating percentage of 5 subjects
+// Calculating percentage of 5 subjects considering marks out of 100.
+#include <stdio.h>
+
+int main()
+{
+    float Mat_marks, Phy_marks, Che_marks, Engl_marks, Com_marks;
+    printf("Enter Your Marks of Maths: ");
+    scanf("%f", &Mat_marks);
+    printf("Enter Your Marks of Physics: ");
+    scanf("%f", &Phy_marks);
+    printf("Enter Your Marks of Chemistry: ");
+    scanf("%f", &Che_marks);
+    printf("Enter Your Marks of English: ");
+    scanf("%f", &Engl_marks);
+    printf("Enter Your Marks of Computer: ");
+    scanf("%f", &Com_marks);
+
+    float total_marks = Mat_marks + Phy_marks + Che_marks + Engl_marks + Com_marks;
+    float Maximum_marks = 5 * 100.0;
+
+    printf("Your Score is: %.2f%%\n", (total_marks / Maximum_marks) * 100);
+
+    return 0;
+}
