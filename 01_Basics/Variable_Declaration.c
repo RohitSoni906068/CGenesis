7f238ab6b36df643f186d6f0cd525f64a885968a
rohit528590 <rohit528590@icloud.com>
2025-07-09 10:30:30 +0530

    Basics Of C

---
+++ 01_Basics/Variable_Declaration.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+int main(){
+    int a; // Variable declaration
+    a = 6; // Variable initialization
+
+    int b=6; // Varible declaration+initialization
+
+    int c=4,d=5; // Multiply Variable declaration+initialization
+    printf("The output of this program is %d %d %d %d", a,b,c,d); // "%d" is a format specifier used in input/output functions like printf() and scanf().
+    return 0;
+}
