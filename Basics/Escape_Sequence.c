78d890d8641b6a052ef4cc8092b5d273bf3c2f74
rohit528590 <rohit528590@icloud.com>
2025-07-09 10:28:19 +0530

    Deleted all folders

--- Basics/Escape_Sequence.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-int main()
-{
-    printf(" Hello World \n");           // "\n" is used to move the cursor to the next line in the output
-    printf(" Rohit! ");                  // Rohit Print after printing "Hello World " they are not print together becasue of '\n'
-    printf(" Hell World\n I am Rohit "); // Hello World and Hello Rohit will be print in two line becasue of '\n'
-    return 0;
-}
