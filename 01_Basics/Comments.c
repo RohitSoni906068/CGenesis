12efaa67fde82b3d40f261daafc683e8f601c333
rohit528590 <rohit528590@icloud.com>
2025-07-09 10:25:50 +0530

    Basics Of C

---
+++ 01_Basics/Comments.c
@@ -0,0 +1,18 @@
+/*
+Program: comments.c
+Author: Rohit Kumar
+Purpose: To Understand about Comments in C.
+*/
+#include<stdio.h>
+
+int main(){
+    
+    // ->: This is Called Single-line Comment
+    printf(" Now, We Learn About Comments ");
+
+    /* ->: This is Called Mutiline Coomment 
+    Comment is not executed by the compiler.
+    The compiler ignores comments completely when excuting the program.
+    */
+    return 0; // This is return 
+}
