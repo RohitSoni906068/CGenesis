42d9d1218c2fd3a772c0af13569b4418cf99a5f4
rohit528590 <rohit528590@icloud.com>
2025-08-01 22:37:14 +0530

    Optimizing Repo

--- 02_Basics Practice Problems/14_Fractional_Part_of_Float_N.c
+++
@@ -1,9 +0,0 @@
-// Take float input and print the of the real number.
-#include<stdio.h>
-int main(){
-    float n;
-    printf(" Enter the Number : ");
-    scanf(" %f",&n);
-    printf(" The Fractional Part of %.3f is %.3f \n",n,n-(int)n);
-    return 0;
-}
