eb5bb8913ea071dcfb0375771a7b249b8df6e1fa
rohit528590 <rohit528590@icloud.com>
2025-08-04 17:05:56 +0530

    Practice Problems Based On Arrays Of C.

---
+++ 12_Array_Practice_Problems/01_Student_Below_Pass_Mark.c
@@ -0,0 +1,34 @@
+// Write a C program that accepts the marks of multiple students and prints the roll numbers of the students
+// who scored below the passing mark (e.g., 35).
+
+#include <stdio.h>
+
+#define PASS_MARK 35 // Define a named constant for clarity
+
+int main()
+{
+    int num_students;
+
+    printf(" Enter number of students : ");
+    scanf(" %d", &num_students);
+
+    int marks[num_students];
+
+    for (int i = 0; i < num_students; i++)
+    {
+        printf(" Enter marks for student %d : ", i + 1);
+        scanf(" %d", &marks[i]);
+    }
+
+    printf("\nStudents who scored less than %d :\n", PASS_MARK);
+
+    for (int i = 0; i < num_students; i++)
+    {
+        if (marks[i] < PASS_MARK)
+        {
+            printf(" Roll Number : %d\n", i + 1);
+        }
+    }
+
+    return 0;
+}
