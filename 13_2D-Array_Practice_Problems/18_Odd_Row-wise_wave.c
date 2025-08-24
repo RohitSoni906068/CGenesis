419fd04f1c0e08f62ad0c5c07fd1f638492630fa
rohit528590 <rohit528590@icloud.com>
2025-08-24 08:15:27 +0530

    Practice Problems Based On 2D-Arrays Of C.

---
+++ 13_2D-Array_Practice_Problems/18_Odd_Row-wise_wave.c
@@ -0,0 +1,53 @@
+/*
+Program to print a given matrix(n*m) in odd row-wise wave form.
+Example (rows = 3, cols = 3)
+Input Matrix:        Wave-printed Output:
+1    2   3           3   2   1
+4    5   6           4   5   6
+7    8   9           9   8   7
+*/
+#include <stdio.h>
+
+int main()
+{
+    int rows, cols;
+
+    printf("Enter number of rows in the matrix : ");
+    scanf(" %d", &rows);
+
+    printf("Enter number of columns in the matrix : ");
+    scanf(" %d", &cols);
+
+    int matrix[rows][cols];
+
+    printf("Enter all %d elements of the matrix -:\n", rows * cols);
+    for (int r = 0; r < rows; r++)
+    {
+        for (int c = 0; c < cols; c++)
+        {
+            scanf(" %d", &matrix[r][c]);
+        }
+    }
+
+    printf("Wave-printed matrix -:\n");
+    for (int r = 0; r < rows; r++)
+    {
+        if (r % 2 == 0)
+        {
+            for (int c = cols - 1; c >= 0; c--)
+            {
+                printf("%d ", matrix[r][c]);
+            }
+        }
+        else
+        {
+            for (int c = 0; c < cols; c++)
+            {
+                printf("%d ", matrix[r][c]);
+            }
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
