ad30633afc984fd0a95dabe3fd25c2708d9d813f
rohit528590 <rohit528590@icloud.com>
2025-08-26 16:08:35 +0530

    Practice Problems Based On 2D-Arrays Of C.

---
+++ 13_2D-Array_Practice_Problems/22_In_Place_Verticle_Flip_wave.c
@@ -0,0 +1,54 @@
+/*
+In-place program to transform a given matrix (n x m) by reversing the order of rows (vertical flip).
+Input Matrix:               Transformed Matrix:
+1   2   3   4               9   10  11  12
+5   6   7   8               5   6   7   8
+9   10  11  12              1   2   3   4
+*/
+#include <stdio.h>
+
+int main()
+{
+    int rows, cols;
+    printf("Enter number of rows in the matrix : ");
+    scanf(" %d", &rows);
+    printf("Enter number of columns in the matrix : ");
+    scanf(" %d", &cols);
+
+    int matrix[rows][cols];
+    printf("Enter all %d elements of the matrix -:\n", rows * cols);
+
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < cols; j++)
+        {
+            scanf(" %d", &matrix[i][j]);
+        }
+    }
+
+    // Reverse the rows (vertical flip)
+    int top = 0, bottom = rows - 1;
+    while (top < bottom)
+    {
+        for (int j = 0; j < cols; j++)
+        {
+            int temp = matrix[top][j];
+            matrix[top][j] = matrix[bottom][j];
+            matrix[bottom][j] = temp;
+        }
+        top++;
+        bottom--;
+    }
+
+    printf("Transformed matrix -:\n");
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < cols; j++)
+        {
+            printf("%d ", matrix[i][j]);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
