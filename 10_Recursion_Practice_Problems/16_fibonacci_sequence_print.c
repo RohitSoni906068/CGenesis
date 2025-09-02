72fd3e79ee6a7419bb5be88de65ae75feb62fe10
rohit528590 <rohit528590@icloud.com>
2025-09-02 21:37:19 +0530

    Practice Problems Based On Recursive Function Of C.

--- 10_Recursion_Practice_Problems/16_fibonacci_sequence_print.c
+++ 10_Recursion_Practice_Problems/16_fibonacci_sequence_print.c
@@ -1,32 +1,35 @@
 // Print Fibonacci Sequence from 0 to n Using Recursion.
 #include <stdio.h>
 
-int fibonacci(int term)
+int fibonacci(int term) // Function to compute nth Fibonacci number recursively
 {
-    if (term == 0 || term == 1)
-    {
-        return term;
-    }
-    return fibonacci(term - 1) + fibonacci(term - 2);
+    return (term == 0 || term == 1) ? term : fibonacci(term - 1) + fibonacci(term - 2);
 }
 
-void fibonacci_sequence(int x)
+void fibonacci_sequence(int x) // Function to print Fibonacci sequence from 0 to n recursively
 {
-    if (x < 0)
-    {
-        return;
-    }
-
+    if (x < 0) return;
+    
     fibonacci_sequence(x - 1);
     printf("%d ", fibonacci(x));
+
+    return;
 }
+
 int main()
 {
     int term;
-    printf(" Enter how many terms of Fibonacci sequence to print : ");
-    scanf(" %d", &term);
+    printf("Enter how many terms (0 based) of Fibonacci sequence to print : ");
+    scanf("%d", &term);
 
-    printf(" Fibonacci Sequence : ");
+    // Handle invalid ranges/Edge cases
+    if (term < 0)
+    {
+        printf("Please enter a non-negative integer.\n");
+        return 0;
+    }
+
+    printf("Fibonacci Sequence from 0 to %d -:\n", term);
     fibonacci_sequence(term - 1);
 
     printf("\n");
