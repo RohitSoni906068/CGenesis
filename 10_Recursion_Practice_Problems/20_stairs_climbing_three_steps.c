e44f8272444e5a838cd6bfcb29d868799b8d18d1
rohit528590 <rohit528590@icloud.com>
2025-09-02 21:47:53 +0530

    Practice Problems Based On Recursive Function Of C.

--- 10_Recursion_Practice_Problems/20_stairs_climbing_three_steps.c
+++ 10_Recursion_Practice_Problems/20_stairs_climbing_three_steps.c
@@ -1,23 +1,31 @@
-// Program to find the number of ways to climb 'n' stairs,
-// where one can take either 1 or 2 or 3 steps at a time.
+// Program to find the number of ways to climb 'n' stairs, where one can take 1, 2, or 3 steps at a time.
 #include <stdio.h>
 
-int Stair_Path(int stair)
+int Stair_Path(int stairs)
 {
-    if (stair == 2 || stair == 1 || stair == 3)
-    {
-        return (stair == 3) ? 4 : stair;
-    }
-    return Stair_Path(stair - 1) + Stair_Path(stair - 2) + Stair_Path(stair - 3);
+    // Base case
+    if (stairs == 2 || stairs == 1 || stairs == 3) return (stairs == 3) ? 4 : stairs;
+    
+    // Recursive case
+    return Stair_Path(stairs - 1) + Stair_Path(stairs - 2) + Stair_Path(stairs - 3);
 }
 
 int main()
 {
-    int stair;
+    int stairs;
 
-    printf(" Enter Number of Stair : ");
-    scanf(" %d", &stair);
+    printf("Enter number of stairs : ");
+    scanf("%d", &stairs);
 
-    printf(" Number of Ways to climb %d stair is : %d \n", stair, Stair_Path(stair));
+    // Handle invalid ranges/Edge cases
+    if (stairs < 0)
+    {
+        printf("Please enter a non-negative integer.\n");
+        return 0;
+    }
+
+    int total_ways = Stair_Path(stairs);
+    printf("Number of Ways to climb %d stairs is : %d \n", stairs, total_ways);
+
     return 0;
 }
