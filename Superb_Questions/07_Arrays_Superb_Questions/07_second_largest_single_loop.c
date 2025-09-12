3bfd3497b17dd3facdf40deb9f5c4942b7e0cde4
rohit528590 <rohit528590@icloud.com>
2025-09-12 15:40:38 +0530

    Important Questions From Arrays.

---
+++ Superb_Questions/07_Arrays_Superb_Questions/07_second_largest_single_loop.c
@@ -0,0 +1,33 @@
+// Program to find the second largest element in an array using only one loop.
+#include <stdio.h>
+#include <limits.h>
+
+int main()
+{
+    int secondMax, max = INT_MIN;
+    int arraySize;
+
+    printf("Enter the size of the array : ");
+    scanf("%d", &arraySize);
+
+    int array[arraySize];
+
+    for (int i = 0; i < arraySize; i++)
+    {
+        printf("Enter value for element %d : ", i);
+        scanf("%d", &array[i]);
+
+        // Update max if a greater element is found
+        if (array[i] > max)
+        {
+            secondMax = max; // Update secondMax to current max
+            max = array[i]; // Update max to current element
+        }
+
+        // Update secondMax if a greater element is found
+        else if (array[i] != max && array[i] > secondMax) secondMax = array[i];
+    }
+
+    printf("Second maximum element in the given array is : %d\n", secondMax);
+    return 0;
+}
---
+++ Superb_Questions/07_Arrays_Superb_Questions/12_search_number_in_array.c
@@ -0,0 +1,37 @@
+// Program to check whether a given number is present in an array.
+#include <stdio.h>
+
+int main()
+{
+    int array_size;
+    printf("Enter the size of the array : ");
+    scanf("%d", &array_size);
+
+    int array[array_size];
+
+    for (int i = 0; i < array_size; i++)
+    {
+        printf("Enter value for element %d : ", i);
+        scanf("%d", &array[i]);
+    }
+
+    int x;
+    printf("Enter the number to search for : ");
+    scanf("%d", &x);
+
+    int found = 0;
+    for (int j = 0; j < array_size; j++)
+    {
+        if (array[j] == x)
+        {
+            printf("Yes, the value %d is present at index %d.\n", x, j);
+            found = 1;
+            break;
+        }
+    }
+
+    // if found is not equal to 1.
+    if (!found) printf("No, the value %d is not present in the array.\n", x);
+
+    return 0;
+}
