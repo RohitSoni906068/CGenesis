706e898393fed25204bfe5dd65ffaf6adf8cac4c
rohit528590 <rohit528590@icloud.com>
2025-09-13 07:45:07 +0530

    Important Questions Of Pattern Printing.

---
+++ Superb_Questions/04_Pattern_Printing_Superb_Questions/09_floyds_triangle_numbers.c
@@ -0,0 +1,33 @@
+/*
+15. Floyd's Triangle Pattern (Numbers)
+Program to print Floyd's triangle pattern using consecutive numbers.
+Example : rows = 4
+Expected Output:
+1
+2 3
+4 5 6
+7 8 9 10
+*/
+#include <stdio.h>
+
+int main()
+{
+    int rows;
+    printf("Enter the number of rows : ");
+    scanf("%d", &rows);
+
+    int consecutiveNumber = 1;
+
+    printf("Floyd's Triangle(%d rows) Pattern with Numbers -:\n", rows);
+    for (int i = 1; i <= rows; i++)
+    {
+        for (int j = 1; j <= i; j++)
+        {
+            printf("%d ", consecutiveNumber++);
+        }
+
+        printf("\n");
+    }
+
+    return 0;
+}
---
+++ Superb_Questions/04_Pattern_Printing_Superb_Questions/11_left_rhombus_pattern.c
@@ -0,0 +1,42 @@
+/*
+19. Left-Aligned Rhombus Pattern
+Program to print a left-aligned rhombus pattern using asterisks.
+Example : rows = 4
+Expected Output:
+_ _ _ * * * *
+_ _ * * * *
+_ * * * *
+* * * *
+*/
+#include <stdio.h>
+
+int main()
+{
+    int rows;
+    printf("Enter the number of rows : ");
+    scanf("%d", &rows);
+
+    int spaces = rows - 1; // Print leading spaces
+
+    printf("Left-Aligned Rhombus(%d rows) Pattern (Asterisks) -:\n", rows);
+    for (int i = 1; i <= rows; i++)
+    {
+        // Print leading spaces
+        for (int j = 1; j <= spaces; j++)
+        {
+            printf("  ");
+        }
+
+        // Print stars with a space
+        for (int k = 1; k <= rows; k++)
+        {
+            printf("* ");
+        }
+
+        // Prepare for next row
+        printf("\n");
+        spaces--;
+    }
+
+    return 0;
+}
