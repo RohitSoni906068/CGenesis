fadbb716e0c1c4860ab2cf346839ef6fa2d85e9b
rohit528590 <rohit528590@icloud.com>
2025-09-19 07:05:23 +0530

    Solved challenging Iterative Statements question from CGenesis.

---
+++ Superb_Questions/03_Loops_Superb_Questions/07_fibonacci_series_n_terms.c
@@ -0,0 +1,29 @@
+// Program to print the Fibonacci series from the first term to the nth term.
+#include <stdio.h>
+
+int main()
+{
+    int term;
+    printf("Enter which term of the Fibonacci series you want : ");
+    scanf("%d", &term);
+
+    int previous_term = 0;
+    int last_previous_term = 1;
+    int fibonacci_term = 0;
+    
+    printf("The is You Fibonacci Series 0th Term to %dth Term -> ", term);
+
+    printf("%d ", previous_term);
+    printf("%d ", last_previous_term);
+
+    for (int i = 2; i <= term; i++)
+    {
+        fibonacci_term = previous_term + last_previous_term;
+        previous_term = last_previous_term;
+        last_previous_term = fibonacci_term;
+        printf("%d ", fibonacci_term);
+    }
+    
+    printf("\n");
+    return 0;
+}
