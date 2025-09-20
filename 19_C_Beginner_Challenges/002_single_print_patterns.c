64ca9f87c82b1d2c946c6a39ef449f0016aff870
rohit528590 <rohit528590@icloud.com>
2025-09-20 20:28:41 +0530

    Solved C Beginner Challenges 💯.

---
+++ 19_C_Beginner_Challenges/002_single_print_patterns.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+
+int main() 
+{
+  printf("Right Half Pyramid\n*\n* *\n* * *\n* * * *\n* * * * *\n");
+  printf("Reverse Right Half Pyramid\n* * * * *\n* * * *\n* * *\n* *\n*\n");
+  printf("Left Half Pyramid\n        *\n      * *\n    * * *\n  * * * *\n* * * * *\n");
+  
+  return 0;
+}
